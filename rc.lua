f1a9220009ed1bf9f7bd02111ee5108a12f5ee9c
shtwzrd <2846522+shtwzrd@users.noreply.github.com>
2020-01-07 08:00:56 +0100

    Enable correlation for fennel compiler

--- rc.lua
+++ rc.lua
@@ -1,4 +1,8 @@
 fennel = require("fennel")
+local searcher = fennel.makeSearcher({
+  correlate = true,
+  useMetadata = true
+})
 fennel.path = fennel.path .. ";.config/awesome/?.fnl"
-table.insert(package.loaders or package.searchers, fennel.searcher)
+table.insert(package.loaders or package.searchers, searcher)
 require("init") -- load ~/.config/awesome/init.fnl
